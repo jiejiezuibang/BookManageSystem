1bd3877311cd655ca9fa59206c71d5081f61d541
jiejie <2842084118@qq.com>
2022-07-25 16:44:15 +0800

    v1.2(优化主页面状态栏)

--- BookSystem/BookMainForm.Designer.cs
+++ BookSystem/BookMainForm.Designer.cs
@@ -31,88 +31,35 @@ namespace BookSystem
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BookMainForm));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panIndex = new System.Windows.Forms.Panel();
-            this.stMain = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.msMain = new System.Windows.Forms.MenuStrip();
             this.图书管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.读者管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.借还记录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.图书ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.stMain = new System.Windows.Forms.StatusStrip();
+            this.tsrTopMain = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.stMain.SuspendLayout();
             this.msMain.SuspendLayout();
+            this.stMain.SuspendLayout();
+            this.tsrTopMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.tsrTopMain);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(942, 81);
             this.panel1.TabIndex = 0;
-            // 
-            // button4
-            // 
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button4.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(348, 0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(120, 81);
-            this.button4.TabIndex = 3;
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(232, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 81);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(112, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 81);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 81);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
@@ -128,10 +75,58 @@ namespace BookSystem
             // panIndex
             // 
             this.panIndex.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panIndex.Location = new System.Drawing.Point(0, 30);
+            this.panIndex.Location = new System.Drawing.Point(0, 28);
             this.panIndex.Name = "panIndex";
-            this.panIndex.Size = new System.Drawing.Size(942, 561);
+            this.panIndex.Size = new System.Drawing.Size(942, 563);
             this.panIndex.TabIndex = 2;
+            // 
+            // msMain
+            // 
+            this.msMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.图书管理ToolStripMenuItem,
+            this.读者管理ToolStripMenuItem,
+            this.借还记录ToolStripMenuItem,
+            this.图书ToolStripMenuItem});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(942, 28);
+            this.msMain.TabIndex = 0;
+            this.msMain.Text = "menuStrip1";
+            // 
+            // 图书管理ToolStripMenuItem
+            // 
+            this.图书管理ToolStripMenuItem.Name = "图书管理ToolStripMenuItem";
+            this.图书管理ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.图书管理ToolStripMenuItem.Text = "图书管理";
+            this.图书管理ToolStripMenuItem.Click += new System.EventHandler(this.图书管理ToolStripMenuItem_Click);
+            // 
+            // 读者管理ToolStripMenuItem
+            // 
+            this.读者管理ToolStripMenuItem.Name = "读者管理ToolStripMenuItem";
+            this.读者管理ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.读者管理ToolStripMenuItem.Text = "读者管理";
+            this.读者管理ToolStripMenuItem.Click += new System.EventHandler(this.读者管理ToolStripMenuItem_Click);
+            // 
+            // 借还记录ToolStripMenuItem
+            // 
+            this.借还记录ToolStripMenuItem.Name = "借还记录ToolStripMenuItem";
+            this.借还记录ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.借还记录ToolStripMenuItem.Text = "借还记录";
+            this.借还记录ToolStripMenuItem.Click += new System.EventHandler(this.借还记录ToolStripMenuItem_Click);
+            // 
+            // 图书ToolStripMenuItem
+            // 
+            this.图书ToolStripMenuItem.Name = "图书ToolStripMenuItem";
+            this.图书ToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.图书ToolStripMenuItem.Text = "图书借还";
+            this.图书ToolStripMenuItem.Click += new System.EventHandler(this.图书ToolStripMenuItem_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(54, 20);
+            this.toolStripStatusLabel1.Text = "已登录";
             // 
             // stMain
             // 
@@ -144,53 +139,55 @@ namespace BookSystem
             this.stMain.TabIndex = 1;
             this.stMain.Text = "已登录";
             // 
-            // toolStripStatusLabel1
+            // tsrTopMain
             // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(54, 20);
-            this.toolStripStatusLabel1.Text = "已登录";
+            this.tsrTopMain.ImageScalingSize = new System.Drawing.Size(80, 60);
+            this.tsrTopMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.tsrTopMain.Location = new System.Drawing.Point(0, 0);
+            this.tsrTopMain.Name = "tsrTopMain";
+            this.tsrTopMain.Size = new System.Drawing.Size(942, 87);
+            this.tsrTopMain.TabIndex = 1;
+            this.tsrTopMain.Text = "toolStrip1";
             // 
-            // msMain
+            // toolStripButton1
             // 
-            this.msMain.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.图书管理ToolStripMenuItem,
-            this.读者管理ToolStripMenuItem,
-            this.借还记录ToolStripMenuItem,
-            this.图书ToolStripMenuItem});
-            this.msMain.Location = new System.Drawing.Point(0, 0);
-            this.msMain.Name = "msMain";
-            this.msMain.Size = new System.Drawing.Size(942, 30);
-            this.msMain.TabIndex = 0;
-            this.msMain.Text = "menuStrip1";
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(104, 84);
+            this.toolStripButton1.Text = "toolStripButton1";
             // 
-            // 图书管理ToolStripMenuItem
+            // toolStripButton2
             // 
-            this.图书管理ToolStripMenuItem.Name = "图书管理ToolStripMenuItem";
-            this.图书管理ToolStripMenuItem.Size = new System.Drawing.Size(83, 26);
-            this.图书管理ToolStripMenuItem.Text = "图书管理";
-            this.图书管理ToolStripMenuItem.Click += new System.EventHandler(this.图书管理ToolStripMenuItem_Click);
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(104, 84);
+            this.toolStripButton2.Text = "toolStripButton2";
             // 
-            // 读者管理ToolStripMenuItem
+            // toolStripButton3
             // 
-            this.读者管理ToolStripMenuItem.Name = "读者管理ToolStripMenuItem";
-            this.读者管理ToolStripMenuItem.Size = new System.Drawing.Size(83, 26);
-            this.读者管理ToolStripMenuItem.Text = "读者管理";
-            this.读者管理ToolStripMenuItem.Click += new System.EventHandler(this.读者管理ToolStripMenuItem_Click);
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(104, 84);
+            this.toolStripButton3.Text = "toolStripButton3";
             // 
-            // 借还记录ToolStripMenuItem
+            // toolStripButton4
             // 
-            this.借还记录ToolStripMenuItem.Name = "借还记录ToolStripMenuItem";
-            this.借还记录ToolStripMenuItem.Size = new System.Drawing.Size(83, 26);
-            this.借还记录ToolStripMenuItem.Text = "借还记录";
-            this.借还记录ToolStripMenuItem.Click += new System.EventHandler(this.借还记录ToolStripMenuItem_Click);
-            // 
-            // 图书ToolStripMenuItem
-            // 
-            this.图书ToolStripMenuItem.Name = "图书ToolStripMenuItem";
-            this.图书ToolStripMenuItem.Size = new System.Drawing.Size(83, 26);
-            this.图书ToolStripMenuItem.Text = "图书借还";
-            this.图书ToolStripMenuItem.Click += new System.EventHandler(this.图书ToolStripMenuItem_Click);
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(104, 84);
+            this.toolStripButton4.Text = "toolStripButton4";
             // 
             // BookMainForm
             // 
@@ -203,13 +200,17 @@ namespace BookSystem
             this.Name = "BookMainForm";
             this.Text = "图书管理系统";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BookMainForm_FormClosing);
+            this.Load += new System.EventHandler(this.BookMainForm_Load);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.stMain.ResumeLayout(false);
-            this.stMain.PerformLayout();
             this.msMain.ResumeLayout(false);
             this.msMain.PerformLayout();
+            this.stMain.ResumeLayout(false);
+            this.stMain.PerformLayout();
+            this.tsrTopMain.ResumeLayout(false);
+            this.tsrTopMain.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -217,18 +218,19 @@ namespace BookSystem
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.MenuStrip msMain;
         private System.Windows.Forms.ToolStripMenuItem 图书管理ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 读者管理ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 借还记录ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 图书ToolStripMenuItem;
-        private System.Windows.Forms.StatusStrip stMain;
         private System.Windows.Forms.Panel panIndex;
+        private System.Windows.Forms.StatusStrip stMain;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStrip tsrTopMain;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
     }
 }
--- BookSystem/BookMainForm.cs
+++ BookSystem/BookMainForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,40 @@ namespace BookSystem
 {
     public partial class BookMainForm : Form
     {
+        // 定义一个委托 
+        private delegate void SetDateTime();
+        /// <summary>
+        /// 设置窗体的状态栏信息标签
+        /// </summary>
+        private void toolStatusLable()
+        {
+            // 创建线程
+            Thread thread = new Thread(new ThreadStart(DateTimeInfo)); // 线程函数调用
+            // 设置为后台线程 后台线程：应用程序可以不考虑其是否已经运行完毕而直接退出，
+            // 所有的后台线程在应用程序退出时都会自动结束
+            thread.IsBackground = true;
+            thread.Start(); // 开启线程
+        }
+        // 线程函数调用
+        private void DateTimeInfo()
+        {
+            while (true) // 死循环
+            {
+                SetDateTime setDateTime = new SetDateTime(
+                    delegate // 匿名委托
+                    {
+                        //显示窗口登录状态信息
+                        this.toolStripStatusLabel1.Text = "状态：" + this.Tag + "已登录."
+                        + "                                                 " +
+                        " 欢迎使用该系统 "
+                        + "                                                 " +
+                        DateTime.Now.ToString();
+                    }
+                );
+                // 调用该委托
+                setDateTime();
+            }
+        }
         // 单例模式
         public static BookMainForm bookMainForm;
         // 获取BookMainForm页面
@@ -96,6 +131,16 @@ namespace BookSystem
         {
             // 关闭主程序
             Application.Exit();
+        }
+        /// <summary>
+        /// 窗体加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BookMainForm_Load(object sender, EventArgs e)
+        {
+            // 设置窗体的状态栏信息标签
+            toolStatusLable();
         }
     }
 }
